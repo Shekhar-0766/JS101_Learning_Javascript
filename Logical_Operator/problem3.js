1b179b33bf41d85190571874c4ec16b31afb6653
Chandra Shekhar Chaudhary <112809711+Shekhar-0766@users.noreply.github.com>
2022-09-05 17:50:20 +0530

    Logical Operator Problem Added

---
+++ Logical_Operator/problem3.js
@@ -0,0 +1,10 @@
+// Given and character if it is a consonant print "Consonant"
+
+var ch="a";
+
+if(ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u"){
+  console.log("Vowel");
+}
+else{
+  console.log("Consonant");
+}
---
+++ Logical_Operator/problem5.js
@@ -0,0 +1,12 @@
+// Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+
+let day= "fri";
+
+day=="sun" ? console.log("Sunday") 
+  : day=="mon" ? console.log("Monday")
+   : day=="tue" ? console.log("Tuesday")
+    : day=="wed" ? console.log("Wednesday")
+      : day=="thur" ? console.log("Thurday")
+        : day=="fri" ? console.log("Friday")
+          : day=="sat" ? console.log("Saturday")
+            : console.log("Invalid Keywords")
