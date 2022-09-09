2790f178506ae801d4efd55742520ce634c41ab3
Chandra Shekhar Chaudhary <112809711+Shekhar-0766@users.noreply.github.com>
2022-09-09 23:20:19 +0530

    While_Loop_Increment_decrement problems added.

---
+++ While_Loop_Increment_decrement/problem1.js
@@ -0,0 +1,9 @@
+// Print the numbers from the given starting point till ending point (including both start and end)
+
+let i=0;
+let ending_point=10;
+
+while(i<=ending_point){
+  console.log(i);
+  i++;
+}
